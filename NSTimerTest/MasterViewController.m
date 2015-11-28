0311e245018f581b0da411bebb7fc3057968c351
Pablo Padilla <pgpb.padilla@gmail.com>
2015-11-28 10:14:58 -0800

    new: dev: Test NSTimer to update label text.

--- NSTimerTest/MasterViewController.m
+++ NSTimerTest/MasterViewController.m
@@ -40,7 +40,7 @@
     if (!self.objects) {
         self.objects = [[NSMutableArray alloc] init];
     }
-    [self.objects insertObject:[NSDate date] atIndex:0];
+    [self.objects insertObject:[[NSDate date] dateByAddingTimeInterval:6000] atIndex:0];
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
